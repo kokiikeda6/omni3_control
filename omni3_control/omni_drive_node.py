379e04302022e2889997b7a3ea1ebc7b62317840
Koki Ikeda <s21c1010gm@s.chibakoudai.jp>
2025-12-17 18:11:24 +0900

    add timer callback

--- omni3_control/omni_drive_node.py
+++ omni3_control/omni_drive_node.py
@@ -13,23 +13,41 @@ class OmniDriveNode(Node):
         self.STOP = 375
         #self.theta = [0, 2*pi/3, -2*pi/3]
         self.theta = [pi/2, -pi/6, -5*pi/6]
-        self.sub = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 1)
 
 	# サブスクライバ: 値を保存するだけ
-        self.sub = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
+        self.sub = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 1)
 
         # タイマー: 10Hz (0.1秒) ごとに計算・出力
         timer_period = 0.1  
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
+        self.current_vx = 0
+        self.current_vy = 0
+        self.current_omega = 0
+
+        # 通信途絶監視用の変数（現在時刻で初期化）
+        self.last_msg_time = self.get_clock().now()
+        self.timeout_duration = 0.5 # 0.5秒以上途切れたら停止
+
     def cmd_vel_callback(self, msg: Twist):
         self.current_vx = msg.linear.x
         self.current_vy = msg.linear.y
         self.current_omega = msg.angular.z
+
+        # メッセージ受信時刻を更新
+        self.last_msg_time = self.get_clock().now()  
+ 
     def timer_callback(self):
-        vx = self.current_vx = msg.linear.x
-        vy = self.current_vy = msg.linear.y
-        omega = self.current_omega = msg.angular.z
+        # 現在時刻と最後に受信した時刻の差を計算
+        elapsed_time = (self.get_clock().now() - self.last_msg_time).nanoseconds / 1e9
+
+        # タイムアウト判定
+        if elapsed_time > self.timeout_duration:
+            vx, vy, omega = 0.0, 0.0, 0.0
+        else:
+            vx = self.current_vx
+            vy = self.current_vy
+            omega = self.current_omega
 
         motor_sign = [1, 1, 1]  # 各モータの回転方向の符号
         pwm_values = []
